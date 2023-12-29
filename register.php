02202614c1e4f2649013de329eefbca818e95357
unknown <ahmed.elragal02@gmail.com>
2023-12-29 22:10:38 +0200

    magdy 3ars

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+<?php
 session_start();
 if (isset($_SESSION["SESSION_EMAIL"])) {
   header("Location: profile.php");
@@ -23,7 +24,7 @@ if (isset($_POST["register"])) {
     echo "ssn";
   } else {
     $sql = "INSERT INTO users (SSN,FirstName, LastName, Email, Password, address, city, country,admin)
-      VALUES ('$ssn', '$fname', '$lname','$email', '$password', '$address', '$city', '$country', 0)";
+      VALUES ('$ssn', '$fname', '$lname','$email', '$password', '$address', '$city', '$country', '0')";
     $result = mysqli_query($conn, $sql);
     if ($result) {
       //header("Location: login.html");
@@ -34,3 +35,4 @@ if (isset($_POST["register"])) {
     }
   }
 }
+?>
